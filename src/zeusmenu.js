ccdcbd911636a9fd70e7a37948af705d05f99256
FehGamesBrgit commit -m First <lourencofelipe051@gmail.com>
2021-03-11 17:07:15 -0300

    First commit

---
+++ src/zeusmenu.js
@@ -0,0 +1,39 @@
+const felipemenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Felipe:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *ZEUS*
+  DUVIDAS? 👇
+  WA.me/556993899391
+╚════════════════════`
+}
+
+exports.felipemenu = felipemenu
+
+
+
+
+
+
+
+
